087885f97fb0be84d446ed2722a64449f671b995
Merovizian <ericgmicaela@gmail.com>
2022-07-02 11:57:36 -0300

    Criação de uma estrutura de registro de conversa Emissor, Receptor. V2

--- Test1/Registro.py
+++ Test1/Registro.py
@@ -1,8 +1,9 @@
 from time import time
 from datetime import date, datetime
 from pathlib import Path
+import os
 
-#Emissor é quem tem a conta no Materic
+'''#Emissor é quem tem a conta no Materic
 emissor = input("Quem é o usúario do Materic: ")
 
 #O nome do receptor(receptor de texto) é o nome do seu contato
@@ -17,8 +18,13 @@ data_atual = str(date.today())
 time_atual = time()
 texto = list()
 contador = 0
-horario = datetime.now().strftime("%H:%M:%S")
+horario = datetime.now().strftime("%H:%M:%S")'''
 
+def exibirHistorico(emissor, receptor):
+    arquivotexto = open(f'./{emissor}/{receptor}', 'r')
+    texto = arquivotexto.read().split("\n")
+    for line in texto:
+        print(line)
 
 def arquivoExiste(receptor,emissor):
     '''
@@ -33,7 +39,6 @@ def arquivoExiste(receptor,emissor):
         return 0
     else:
         return 1
-
 
 def historico(receptor,emissor,data):
     '''Verifica se há um historico no sistema do receptor, se houve, as mensagens do dia de hoje.
@@ -57,87 +62,130 @@ def historico(receptor,emissor,data):
         if (contador >= localizacao and localizacao != -1 ):
             print(line)
 
+def registro(emissor, receptor, mensagem=''):
+    '''Faz o registro das conversas e os salva em disco.
+    :param emissor: é o dono da conta do app
+    :param receptor: é quem vai receber as mensagens
+    :param mensagem: é a mensagem que o emissor irá trocar com o receptor
+    :return:Até o momento, não retorna nada.'''
 
-#Verifica se existe arquivo na conta do Emissor, se tiver ele abre, se não tiver, ele cria.
-if arquivoExiste(receptor,emissor) == 0:
-    a = open(f'./{emissor}/{receptor}', 'wt+')
-    print(f"Arquivo {receptor} criado com sucesso!")
-else:
-    a = open(f'./{emissor}/{receptor}', 'a')
-    print(f"Arquivo {receptor} já existente")
-
-#Verifica se existe arquivo na conta do Receptor, se tiver ele abre, se não tiver, ele cria.
-if arquivoExiste(emissor,receptor) == 0:
-    b = open(f'./{receptor}/{emissor}', 'wt+')
-    print(f"Arquivo {emissor} criado com sucesso!")
-else:
-    b = open(f'./{receptor}/{emissor}', 'a')
-    print(f"Arquivo {emissor} já existente")
+    # Cria a pasta do usuario, caso a pasta não exista
+    Path(f'./{emissor}').mkdir(exist_ok=True)
+    # Cria a pasta do usuario, caso a pasta não exista
+    Path(f'./{receptor}').mkdir(exist_ok=True)
 
 
 
+    # Verifica se existe arquivo na conta do Emissor, se tiver ele abre, se não tiver, ele cria.
+    if arquivoExiste(receptor, emissor) == 0:
+        a = open(f'./{emissor}/{receptor}', 'wt+')
+    else:
+        a = open(f'./{emissor}/{receptor}', 'a')
+
+    # Verifica se existe arquivo na conta do Receptor, se tiver ele abre, se não tiver, ele cria.
+    if arquivoExiste(emissor, receptor) == 0:
+        b = open(f'./{receptor}/{emissor}', 'wt+')
+    else:
+        b = open(f'./{receptor}/{emissor}', 'a')
 
 
-#Faz a busca da data no sistema, se a data não existe ele cria uma linha com a data Para o Emissor.
-if ((open(f'./{emissor}/{receptor}', 'r').read().find(dia)) == -1):
-    a.writelines(f"{dia}\n")
+    #CRIA UMA VARIAVEL DE DATA TEMPORARIO PARA PODER TESTAR O SISTEMA
+    dia = input("qual a data de hoje: ")
 
-# Faz a busca da data no sistema, se a data não existe ele cria uma linha com a data Para o RECEPTOR
-if ((open(f'./{receptor}/{emissor}', 'r').read().find(dia)) == -1):
-    b.writelines(f"{dia}\n")
+    # Faz a busca da data no sistema, se a data não existe ele cria uma linha com a data Para o Emissor.
+    if ((open(f'./{emissor}/{receptor}', 'r').read().find(dia)) == -1):
+        a.writelines(f"{dia}\n")
 
+    # Faz a busca da data no sistema, se a data não existe ele cria uma linha com a data Para o RECEPTOR
+    if ((open(f'./{receptor}/{emissor}', 'r').read().find(dia)) == -1):
+        b.writelines(f"{dia}\n")
 
-
-#Apresenta o historico desse usuário
-if (contador >= 0):
+    # Apresenta o historico desse usuário
     historico(receptor, emissor, dia)
-    contador = 2
+
+    # Estrutura de teste, para simular uma conversa. O que as variaveis uteis: recepitor, emissor, mensagem, horario;
+    while True:
+        escolha = input(f"Quem esta escrevendo [1 - {emissor} 2 - {receptor} 3 - Sair do chat]: ")
+        if (escolha == "1"):
+            a = open(f'./{emissor}/{receptor}', 'a')
+            mensagem = input(f"{emissor}: ")
+            horario = datetime.now().strftime("%H:%M:%S")
+            time_atual = time()
+            a.writelines(f"{horario} {emissor}: {mensagem}\n")
+            a.close()
+
+            # Faz as alterações no usuario receptor TAMBEM
+            b = open(f'./{receptor}/{emissor}', 'a')
+            b.writelines(f"{horario} {emissor}: {mensagem}\n")
+            b.close()
+
+        if (escolha == "2"):
+            a = open(f'./{emissor}/{receptor}', 'a')
+
+            mensagem = input(f"{receptor}: ")
+            horario = datetime.now().strftime("%H:%M:%S")
+            time_atual = time()
+            a.writelines(f"{horario} {receptor}: {mensagem}\n")
+            a.close()
+
+            b = open(f'./{receptor}/{emissor}', 'a')
+            b.writelines(f"{horario} {receptor}: {mensagem}\n")
+            b.close()
+
+        if (escolha == "3"):
+            a.close()
+            break
+
+def menu():
+    '''Cria um menu de interação'''
+    print(f"\033[;1m{'MATERIC - TROCA DE MENSAGENS':*^70}\033[m")
+    print("DIGITE A OPÇÃO DESEJADA")
+    print("1 - login")
+    print("2 - cadastro de nova conta")
+
+def login():
+    '''Cria um menu de interação'''
+    os.system('cls')
+    print(f"\033[;1m{'MATERIC - LOGIN'}\033[m")
+    emissor = input("Informe o nome do usuario: ")
+    usuario(emissor)
 
 
-#Estrutura de teste, para simular uma conversa. O que as variaveis uteis: recepitor, emissor, mensagem, horario;
+def usuario(emissor):
+    while True:
+        os.system('cls')
+        print(f"\033[;1m{'MATERIC - TROCA DE MENSAGENS':*^70}\033[m")
+        print(f"BEM VINDO {emissor}")
+        print("DIGITE A OPÇÃO DESEJADA")
+        print("1 - Trocar mensagens: ")
+        print("2 - Exibir histórico: ")
+        print("0 - Sair da conta")
+        escolha = input("Digite a opção desejada: ")
+        if escolha == "0":
+            break
+        if escolha == "1":
+            receptor = input("Com quem deseja conversar: ")
+            registro(emissor, receptor)
+        if escolha == "2":
+            receptor = input("Historico de qual contato: ")
+            exibirHistorico(emissor, receptor)
+            input("Aperte Enter para Sair...")
+
+
+
+
+
+def cadastro():
+    '''Cria um menu de interação'''
+    print(f"\033[;1m{'MATERIC - CADASTRAR NOVA CONTA'}\033[m")
+
 while True:
-    escolha = input(f"Quem esta escrevendo [1 - {emissor} 2 - {receptor} 3 - Sair do chat]: ")
-    if (escolha == "1"):
-        a = open(f'./{emissor}/{receptor}', 'a')
-        mensagem = input(f"{emissor}: ")
-        horario = datetime.now().strftime("%H:%M:%S")
-        time_atual = time()
-        a.writelines(f"{horario} {emissor}: {mensagem}\n")
-        a.close()
-
-        #Faz as alterações no usuario receptor TAMBEM
-        b = open(f'./{receptor}/{emissor}', 'a')
-        b.writelines(f"{horario} {emissor}: {mensagem}\n")
-        b.close()
-
-
-
-
-    if (escolha == "2"):
-        a = open(f'./{emissor}/{receptor}', 'a')
-
-        mensagem = input(f"{receptor}: ")
-        horario = datetime.now().strftime("%H:%M:%S")
-        time_atual = time()
-        a.writelines(f"{horario} {receptor}: {mensagem}\n")
-        a.close()
-
-
-
-        b = open(f'./{receptor}/{emissor}', 'a')
-        b.writelines(f"{horario} {receptor}: {mensagem}\n")
-        b.close()
-
-    if (escolha == "3"):
-        a.close()
-        print(f"Arquivo {receptor} gravado com sucesso!")
-        break
-
-
-'''
-FALTA CRIAR O HISTORICO ANTITESE(salvar o historico na pasta de quem ta recebendo as mensagens.)
-
-'''
-
-
+    menu()
+    choise = input("Digite a opção desejada: ")
+    if (choise== "1"):
+        login()
+    elif (choise == "2"):
+        cadastro()
+    else:
+        choise = input("Opção inválida, por valor escolha a opção correta: ")
 
